8f39d12977c414bb0a33ab6188906f3d57452eec
Mik Zibrov <zdarovzibrov@yandex.ru>
2023-05-18 11:17:24 +0300

    front_back_Fix:18_05

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,8 +5,9 @@ const allowedCors = [
   'https://api.hellomesto.nomoredomains.monster',
   'localhost:3000',
   'http://localhost:3000',
+  'https://localhost:3000',
   'localhost:3001',
-  'http://localhost:3001'
+  'http://localhost:3001',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
