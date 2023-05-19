ec9edb81408c9b47296059916850a38dfecd8867
Mik Zibrov <zdarovzibrov@yandex.ru>
2023-05-19 11:24:04 +0300

    front_back_Fix:new_19_05_new

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,20 +1,26 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация.'));
+    return next(new UnauthorizedError('Необходима Ваша авторизация.'));
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-secret-key');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
+    );
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация.'));
+    return next(new UnauthorizedError('Необходима Ваша авторизация.'));
   }
 
   req.user = payload;
